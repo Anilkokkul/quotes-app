a767bfdd87f3e2e91773e9432b8125fabb524457
Anilkokkul <anilkokkul1997@gmail.com>
2024-01-04 22:35:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 function App() {
+  const [quote, setQuote] = useState({});
+  console.log(quote);
+  const getRandomQuote = async () => {
+    const randomQuote = await axios.get("https://api.quotable.io/random");
+    setQuote(randomQuote.data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote for the Day</h1>
+      <div className="Authors">
+        <Link to={"/authors"}>Authors</Link>
+      </div>
+      <div className="quote">
+        {quote.content && <p>"{quote.content}"</p>}
+        {quote.author && <h4> - {quote.author}</h4>}
+      </div>
+      <button onClick={getRandomQuote}>Get Random Quote</button>
     </div>
   );
 }
---
+++ src/components/GetQuotesByAuthor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GetQuotesByAuthor() {
+  return <div>GetQuotesByAuthor</div>;
+}
+
+export default GetQuotesByAuthor;
